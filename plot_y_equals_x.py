d613abc62213d7601f6b58df66fea3c33d1b1e6e
zoe <18810409365@163.com>
2026-07-25 23:25:13 +0800

    first commit

---
+++ plot_y_equals_x.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""绘制 y = x 的图像"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['PingFang SC', 'Arial Unicode MS']
+plt.rcParams['axes.unicode_minus'] = False
+
+x = np.linspace(-10, 10, 200)
+y = x
+
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.plot(x, y, label='y = x', color='#1f77b4', linewidth=2)
+
+# 画出经过原点的坐标轴
+ax.spines['left'].set_position('zero')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+ax.set_aspect('equal')  # 让 x、y 轴比例一致，直线呈 45°
+ax.set_title('y = x 的图像')
+ax.legend(loc='upper left')
+ax.grid(True, linestyle='--', alpha=0.4)
+
+plt.tight_layout()
+plt.savefig('y_equals_x.png', dpi=150)
+plt.show()
